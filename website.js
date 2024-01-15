669cfdcb0090948261816e64de4460850c027a0e
joshua-zhou <57366452+joshua-zhou14@users.noreply.github.com>
2024-01-15 02:50:47 -0600

    fixed slideshow

--- website.js
+++ website.js
@@ -28,8 +28,17 @@ function showSlides(n) {
 }
 
 // function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+//     let dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
 //     slideIndex++;
 //     if (slideIndex > slides.length) {slideIndex = 1}
-//     showSlides(slideIndex);
-//     setTimeout(showSlides, 3000); // Change image every 2 seconds
-//   }
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 10000); // Change image every 2 seconds
+// }
